380414ac26f70fc66dffc2aca10baa55bc4269a8
umyuu <124dtiaka@gmail.com>
2017-11-16 04:41:13 +0900

    version 0.03
    version 0.03
    ◆change extension name
    DragAndDrop Gestures mini→
    DragAndDrop Gestures Mini
    ◆fix
    a -> img tag

--- src/js/gestures.js
+++ src/js/gestures.js
@@ -6,7 +6,16 @@
             window.addEventListener('dragend', e => { this.ondragend(e); }, false);
         }
         async ondragend(e) {
+            var st = window.performance.now();
             const target = e.target;
+            this.onDownload(target);
+            if(target.parentElement.tagName.toUpperCase() === 'A'){
+                this.onDownload(target.parentElement);
+            }
+            var ed = window.performance.now() - st;
+            //console.log(ed);
+        }
+        onDownload(target){
             const src_attr = target.src || target.href;
             if (src_attr === undefined) {
                 return;
