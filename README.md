cd7f0286e425b574aa6c8c238ecb81f3d05fc93a
umyuu <124dtiaka@gmail.com>
2017-11-16 04:17:00 +0900

    change Repository name
    MouseGestures
    =>
    DragAndDropGesturesMini

---
+++ README.md
@@ -0,0 +1,8 @@
+# MouseGestures
+chrome 拡張。
+画像のリンクをドラック＆ドロップ操作を行うことにより画像ファイルをダウンロード。
+
+## インストール
+[Chromeウェブストア](https://chrome.google.com/webstore/detail/draganddrop-gestures-mini/fenkeapgendkbamkllkaneehllpbobhg?hl=ja)
+## 使い方。
+画像のリンクをドラック＆ドロップ。
---
+++ src/js/gestures.js
@@ -0,0 +1,22 @@
+﻿(function()
+{
+	'use strict';
+    class MouseGestures {
+        constructor() {
+            window.addEventListener('dragend', e => { this.ondragend(e); }, false);
+        }
+        async ondragend(e) {
+            const target = e.target;
+            const src_attr = target.src || target.href;
+            if (src_attr === undefined) {
+                return;
+            }
+            const element = document.createElement('a');
+            element.href = src_attr;
+            // filename
+            element.download = src_attr.split('/').pop();
+            element.click();
+        }
+    }
+    window.ext_mg = new MouseGestures();
+})();
