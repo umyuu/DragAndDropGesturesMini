6bcae4822afdb1b949ce1c4d077ec7d0b43538ea
umyuu <124dtiaka@gmail.com>
2017-11-23 07:23:04 +0900

    chrome.i18n.getMessage auto settings

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -14,10 +14,24 @@ class Popup {
         this.creation_date = new Date();
     }
     generate(){
+        {
+            // i18n
+            // 検索範囲：body要素以下
+            // 検索値：data-i18n-contentが属性に存在すること。
+            // 処理：上記属性が存在時に_locales/(ロケール名)/message.json よりメッセージを取得しtextContentに設定
+            for (let selector of document.querySelectorAll('body *')) {
+                const i18n_content = 'data-i18n-content';
+                if(!selector.hasAttribute(i18n_content)) {
+                    continue;
+                }
+                let attr = selector.getAttribute(i18n_content);
+                selector.textContent = chrome.i18n.getMessage(attr);
+            }
+        }
         // header
         {
             let ele = undefined;
-            let a = document.createElement('A');
+            let a = document.createElement('a');
             a.href = this.Manifest.homepage_url;
             a.target = '_blank';
             a.textContent = this.Manifest.name;
@@ -30,12 +44,10 @@ class Popup {
         //content
         {
             let save = document.querySelector('#save');
-            save.textContent = chrome.i18n.getMessage('popup_Button_Save');
             save.addEventListener('click', (e) => {
                 chrome.downloads.showDefaultFolder();
             }, false);
             let open = document.querySelector('#open');
-            open.textContent = chrome.i18n.getMessage('popup_Button_Open');
             open.addEventListener('click', (e) => {
                 chrome.downloads.showDefaultFolder();
             }, false);
@@ -44,8 +56,6 @@ class Popup {
         {
             let creation_date = document.querySelector('#creation_date');
             creation_date.textContent = this.creation_date;
-            let copy_button = document.querySelector('#copy_button');
-            copy_button.textContent = chrome.i18n.getMessage('popup_Button_Copy');
         }
     }
 }
